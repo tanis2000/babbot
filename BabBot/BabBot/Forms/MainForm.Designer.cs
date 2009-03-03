27b4607d326ac2f87124778a601253a9a27ec2cf
tanis <tanis@bdb5b0f0-0ed8-4a50-bef0-47231d03bae8>
2009-03-03 14:12:39 +0000

    import iniziale

---
+++ BabBot/BabBot/Forms/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace BabBot.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPageMain = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tbLocation = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.lblOrientation = new System.Windows.Forms.Label();
+            this.btnUpdateLocation = new System.Windows.Forms.Button();
+            this.msMain.SuspendLayout();
+            this.tabControlMain.SuspendLayout();
+            this.tabPageMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msMain
+            // 
+            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.msMain.Location = new System.Drawing.Point(0, 0);
+            this.msMain.Name = "msMain";
+            this.msMain.Size = new System.Drawing.Size(521, 24);
+            this.msMain.TabIndex = 0;
+            this.msMain.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "&About..";
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPageMain);
+            this.tabControlMain.Controls.Add(this.tabPage2);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 24);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(521, 291);
+            this.tabControlMain.TabIndex = 1;
+            // 
+            // tabPageMain
+            // 
+            this.tabPageMain.Controls.Add(this.btnUpdateLocation);
+            this.tabPageMain.Controls.Add(this.lblOrientation);
+            this.tabPageMain.Controls.Add(this.lblLocation);
+            this.tabPageMain.Controls.Add(this.textBox2);
+            this.tabPageMain.Controls.Add(this.tbLocation);
+            this.tabPageMain.Controls.Add(this.btnRun);
+            this.tabPageMain.Location = new System.Drawing.Point(4, 22);
+            this.tabPageMain.Name = "tabPageMain";
+            this.tabPageMain.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMain.Size = new System.Drawing.Size(513, 265);
+            this.tabPageMain.TabIndex = 0;
+            this.tabPageMain.Text = "Main";
+            this.tabPageMain.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(6, 6);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run WoW";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // tbLocation
+            // 
+            this.tbLocation.Location = new System.Drawing.Point(75, 36);
+            this.tbLocation.Name = "tbLocation";
+            this.tbLocation.Size = new System.Drawing.Size(190, 20);
+            this.tbLocation.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(75, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(8, 39);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(51, 13);
+            this.lblLocation.TabIndex = 3;
+            this.lblLocation.Text = "Location:";
+            // 
+            // lblOrientation
+            // 
+            this.lblOrientation.AutoSize = true;
+            this.lblOrientation.Location = new System.Drawing.Point(8, 71);
+            this.lblOrientation.Name = "lblOrientation";
+            this.lblOrientation.Size = new System.Drawing.Size(61, 13);
+            this.lblOrientation.TabIndex = 4;
+            this.lblOrientation.Text = "Orientation:";
+            // 
+            // btnUpdateLocation
+            // 
+            this.btnUpdateLocation.Location = new System.Drawing.Point(271, 34);
+            this.btnUpdateLocation.Name = "btnUpdateLocation";
+            this.btnUpdateLocation.Size = new System.Drawing.Size(112, 23);
+            this.btnUpdateLocation.TabIndex = 5;
+            this.btnUpdateLocation.Text = "Update Location";
+            this.btnUpdateLocation.UseVisualStyleBackColor = true;
+            this.btnUpdateLocation.Click += new System.EventHandler(this.btnUpdateLocation_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 315);
+            this.Controls.Add(this.tabControlMain);
+            this.Controls.Add(this.msMain);
+            this.MainMenuStrip = this.msMain;
+            this.Name = "MainForm";
+            this.msMain.ResumeLayout(false);
+            this.msMain.PerformLayout();
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPageMain.ResumeLayout(false);
+            this.tabPageMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPageMain;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblOrientation;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbLocation;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnUpdateLocation;
+    }
+}
+
---
+++ BabBot/BabBot/Forms/MainForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Security;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Magic;
+
+namespace BabBot.Forms
+{
+    public partial class MainForm : Form
+    {
+        private BlackMagic bm;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Process p;
+
+            try
+            {
+                p = Process.Start(@"c:\games\world of warcraft\wow.exe", "", "guest", new SecureString(), "");
+            } 
+            catch(Exception)
+            {
+                MessageBox.Show("Cannot run an instance of WoW", "Error" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (p == null)
+            {
+                MessageBox.Show("Cannot obtain process information", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                bm = new BlackMagic();
+                bm.OpenProcessAndThread(p.Id);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnUpdateLocation_Click(object sender, EventArgs e)
+        {
+            if (bm != null)
+            {
+                bm.SuspendThread(bm.ThreadHandle);
+
+                uint playerBase = bm.ReadUInt(bm.ReadUInt(bm.ReadUInt(0x0127F13C) + 0x30) + 0x28);
+
+                float x = bm.ReadFloat(playerBase + 0x7D0);
+                float y = bm.ReadFloat(playerBase + 0x7D4);
+                float z = bm.ReadFloat(playerBase + 0x7D8);
+
+
+                bm.ResumeThread();
+
+                UInt64 CurTargetGuid = bm.ReadUInt64(0x10A68E0);
+
+                tbLocation.Text = String.Format("Loc: {0}, {1}, {2} | {3}", x, y, z, CurTargetGuid);
+            }
+        }
+    }
+}
