300ba6338462c6ebeb31e650a7ba3abdb2b22b69
ivp4 <ivp4@bdb5b0f0-0ed8-4a50-bef0-47231d03bae8>
2009-12-02 02:55:36 +0000

    Added NPCListForm

---
+++ BabBot/BabBot/Forms/NPCListForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace BabBot.Forms
+{
+    partial class NPCListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbWoWVersion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(12, 334);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(267, 334);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(186, 334);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "WoW Version:";
+            // 
+            // cbWoWVersion
+            // 
+            this.cbWoWVersion.FormattingEnabled = true;
+            this.cbWoWVersion.Location = new System.Drawing.Point(91, 6);
+            this.cbWoWVersion.Name = "cbWoWVersion";
+            this.cbWoWVersion.Size = new System.Drawing.Size(86, 21);
+            this.cbWoWVersion.TabIndex = 31;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "NPC List";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 57);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(165, 271);
+            this.treeView1.TabIndex = 34;
+            // 
+            // NPCListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(354, 369);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbWoWVersion);
+            this.Name = "NPCListForm";
+            this.Controls.SetChildIndex(this.btnHelp, 0);
+            this.Controls.SetChildIndex(this.btnClose, 0);
+            this.Controls.SetChildIndex(this.btnSave, 0);
+            this.Controls.SetChildIndex(this.cbWoWVersion, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.treeView1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbWoWVersion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ BabBot/BabBot/Forms/NPCListForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BabBot.Forms
+{
+    public partial class NPCListForm : BabBot.Forms.GenericDialog
+    {
+        public NPCListForm() : base ("npc_list")
+        {
+            InitializeComponent();
+        }
+    }
+}
