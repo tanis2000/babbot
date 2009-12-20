2098bc1768084f460216f01fd8a8a0183468958b
attn99 <attn99@bdb5b0f0-0ed8-4a50-bef0-47231d03bae8>
2009-12-20 01:37:48 +0000

    Added Route Recorder Form

---
+++ BabBot/BabBot/Forms/RouteRecorderForm.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace BabBot.Forms
+{
+    partial class RouteRecorderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbObjB = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbObjA = new System.Windows.Forms.ComboBox();
+            this.cbTypeA = new System.Windows.Forms.ComboBox();
+            this.cbTypeB = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnControl = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(12, 357);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(365, 357);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(284, 357);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(237, 323);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbObjB);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.cbObjA);
+            this.groupBox1.Controls.Add(this.cbTypeA);
+            this.groupBox1.Controls.Add(this.cbTypeB);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(255, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 278);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Route Details";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 192);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(170, 80);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Route Description:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(44, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Endpoint B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Object:";
+            // 
+            // cbObjB
+            // 
+            this.cbObjB.FormattingEnabled = true;
+            this.cbObjB.Location = new System.Drawing.Point(58, 144);
+            this.cbObjB.Name = "cbObjB";
+            this.cbObjB.Size = new System.Drawing.Size(121, 21);
+            this.cbObjB.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Object:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Type:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Type:";
+            // 
+            // cbObjA
+            // 
+            this.cbObjA.FormattingEnabled = true;
+            this.cbObjA.Location = new System.Drawing.Point(58, 65);
+            this.cbObjA.Name = "cbObjA";
+            this.cbObjA.Size = new System.Drawing.Size(121, 21);
+            this.cbObjA.TabIndex = 4;
+            // 
+            // cbTypeA
+            // 
+            this.cbTypeA.FormattingEnabled = true;
+            this.cbTypeA.Location = new System.Drawing.Point(58, 32);
+            this.cbTypeA.Name = "cbTypeA";
+            this.cbTypeA.Size = new System.Drawing.Size(121, 21);
+            this.cbTypeA.TabIndex = 1;
+            // 
+            // cbTypeB
+            // 
+            this.cbTypeB.FormattingEnabled = true;
+            this.cbTypeB.Location = new System.Drawing.Point(58, 117);
+            this.cbTypeB.Name = "cbTypeB";
+            this.cbTypeB.Size = new System.Drawing.Size(121, 21);
+            this.cbTypeB.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Endpoint A:";
+            // 
+            // btnControl
+            // 
+            this.btnControl.Location = new System.Drawing.Point(365, 296);
+            this.btnControl.Name = "btnControl";
+            this.btnControl.Size = new System.Drawing.Size(75, 23);
+            this.btnControl.TabIndex = 6;
+            this.btnControl.Tag = "false";
+            this.btnControl.Text = "Start";
+            this.btnControl.UseVisualStyleBackColor = true;
+            this.btnControl.Click += new System.EventHandler(this.btnControl_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(284, 296);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // RouteRecorderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(452, 392);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnControl);
+            this.Name = "RouteRecorderForm";
+            this.Text = "Route Recorder";
+            this.Controls.SetChildIndex(this.btnControl, 0);
+            this.Controls.SetChildIndex(this.btnReset, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.dataGridView1, 0);
+            this.Controls.SetChildIndex(this.btnHelp, 0);
+            this.Controls.SetChildIndex(this.btnClose, 0);
+            this.Controls.SetChildIndex(this.btnSave, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbObjB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbObjA;
+        private System.Windows.Forms.ComboBox cbTypeA;
+        private System.Windows.Forms.ComboBox cbTypeB;
+        private System.Windows.Forms.Button btnControl;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ BabBot/BabBot/Forms/RouteRecorderForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using BabBot.Common;
+using BabBot.Wow;
+using BabBot.Manager;
+
+namespace BabBot.Forms
+{
+    
+
+
+    public partial class RouteRecorderForm : BabBot.Forms.GenericDialog
+    {
+        private string[] EndpointList;
+        private Route route = new Route();
+
+        public RouteRecorderForm()
+            : base ("route_mgr")
+        {
+            InitializeComponent();
+
+            EndpointList = new string[DataManager.EndpointsSet.Count];
+            DataManager.EndpointsSet.Keys.CopyTo(EndpointList, 0);
+
+            int idx = Array.IndexOf(EndpointList, "undef");
+            cbTypeA.DataSource = EndpointList;
+            cbTypeA.SelectedIndex = idx;
+            cbTypeB.DataSource = EndpointList;
+            cbTypeB.SelectedIndex = idx;
+
+            dataGridView1.DataSource = route.List;
+        }
+
+        private void btnControl_Click(object sender, EventArgs e)
+        {
+            if (btnControl.Text.Equals("Start"))
+            {
+                // Start recordint
+                // TODO
+                btnControl.Text.Equals("Stop");
+            }
+            else
+            {
+                // Start recordint
+                // TODO
+                btnControl.Text.Equals("Start");
+            }
+        }
+
+        public Route Record(EndpointTypes type_a, string name_a, 
+                                    EndpointTypes type_b, string name_b)
+        {
+            return null;
+        }
+    }
+}
